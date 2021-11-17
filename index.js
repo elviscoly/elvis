b0a870bb6bd85e7d80dddb80bfea83d6b838879e
elviscoly <elviscoly2017@gmail.com>
2021-11-17 22:15:55 +0100

    Format text

--- index.js
+++ index.js
@@ -2,27 +2,36 @@
 
 const welcome = require('cli-welcome');
 const pkgJSON = require('./package.json');
+const chalk = require('chalk');
+
+const log = console.log;
 
 welcome({
-  title: pkgJSON.name,
+  title: `Elvis Iria`,
   tagLine: `Hey, nice to meet you`,
   description: pkgJSON.description,
   version: pkgJSON.version,
-  bgColor: `#FADC00`,
+  bgColor: `#FADC05`,
   color: `#000000`,
   bold: true,
   clear: true,
 
 })
 
-console.log(`
-Elvis Iria
+log(`
 
-Software Engineer at M-rinternational
+${chalk.green(`Software Engineer at M-rinternational`)}
 
-About: I'm a software developer. As you'll see, I have six years plus of hands-on experience in efficiently coding web application and mobile applications using modern HTML5, CSS, JavaScript, PHP, Node JS, Angular, Native script, Ionic, Dart, Flutter, React and others Frameworks. Also specialize using MongoDB, SQL and MYSQL
+${chalk.italic(`
+  About: I'm a software developer. As you'll see, 
+  I have six years plus of hands-on experience in efficiently 
+  coding web application and mobile applications using modern HTML5, 
+  CSS, JavaScript, PHP, Node JS, Angular, Native script, Ionic, Dart, 
+  Flutter, React and others Frameworks. 
+  Also specialize using MongoDB, SQL and MYSQL
+`)}
 
-Twitter: https://twitter.com/elviscoly
-GitHub: https://github.com/elviscoly
+${chalk.hex(`#1da1f2`).bold.inverse(` Twitter `)}: ${chalk.dim(`https://twitter.com/elviscoly`)}
+${chalk.hex(`#6cc644`).bold.inverse(` GitHub `)}: ${chalk.dim(`https://github.com/elviscoly`)}
 
 `);
