494826e97b374d6dfcfac57bd4917a44349b4e77
elviscoly <elviscoly2017@gmail.com>
2021-11-17 15:13:16 +0100

    First Version

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+console.log(`
+Elvis Iria
+
+Software Engineer at M-rinternational
+
+About: I'm a software developer. As you'll see, I have six years plus of hands-on experience in efficiently coding web application and mobile applications using modern HTML5, CSS, JavaScript, PHP, Node JS, Angular, Native script, Ionic, Dart, Flutter, React and others Frameworks. Also specialize using MongoDB, SQL and MYSQL
+
+Twitter: https://twitter.com/elviscoly
+GitHub: https://github.com/elviscoly
+
+`);
